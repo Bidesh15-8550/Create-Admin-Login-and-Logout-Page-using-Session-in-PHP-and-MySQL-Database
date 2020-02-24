6b4d9867f3ba56d645c8048424810585c4389b9d
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-02-24 21:34:03 +0600

    Updated

--- adminmainpage.php
+++ adminmainpage.php
@@ -10,7 +10,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Welcome Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
    
